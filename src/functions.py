8ccab3e01c72d4d66ce128f504e5bd19e1964d0c
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2022-10-21 22:32:38 +0200

    first commit

---
+++ src/functions.py
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+from pytube import Playlist
+import ffmpy
+from termcolor import colored
+import os
+import PySimpleGUI as sg
+import requests
+from PIL import Image
+
+def get_thumbnail(url):
+    file = open("tmp/thumbnail.gif", "wb")
+    file.truncate(0)
+    file.close()
+    r = requests.get(url)
+    with open("tmp/thumbnail.png", "wb") as f:
+        f.write(r.content)
+    im = Image.open("tmp/thumbnail.png")
+    im.save("tmp/thumbnail.gif", "GIF")
+    os.system("rm tmp/thumbnail.png")
+    
+
+def downloading(title, description, url, thumbnail_url):
+    get_thumbnail(thumbnail_url)
+    sg.theme('Black')
+
+    layout = [
+        [sg.Text(title, font=("Latin Modern Mono", 20))],
+        [sg.Image("tmp/thumbnail.gif")],
+        [sg.Text(description, font=("Latin Modern Mono", 15))],
+        [sg.Text("URL: {}".format(url), font=("Latin Modern Mono", 15))],
+        [sg.Text("Downloading...", font=("Latin Modern Mono", 30))]
+    ]
+
+    window = sg.Window("Downloading...", layout, size=(1700, 1000), finalize=True)
+
+    return window
+
+
+def downloadAudio(link, directory):
+    playlist = Playlist(link)
+    for video in playlist.videos:
+        download_page = downloading(video.title, video.description, video.watch_url, video.thumbnail_url)
+        if "//" in video.title or "/" in video.title or " " in video.title:
+            video.title = video.title.replace(" ", "_").replace("//", "_").replace("/", "_")
+        audio = video.streams.get_audio_only()
+        audio.download(filename=video.title + ".mp4", output_path=directory)
+        audioTitle = audio.title
+
+        new_filename = directory + audioTitle + '.mp3'
+        default_filename = directory + audioTitle + '.mp4'
+
+        print(colored('Title : {}\nURL : {}\n\n'.format(video.title, video.watch_url), 'yellow'))
+        ff = ffmpy.FFmpeg(
+            inputs={default_filename : None},
+            outputs={new_filename : None}
+        )
+        ff.run()
+        download_page.close()
+        print(colored("Downloaded : {} with url : {}".format(video.title, video.watch_url), 'green'))
+    command = "rm -rf " + directory + "'*.mp4'"
+    os.system(command)
+    sg.Popup("Downloaded all videos in the playlist.")
+
+def downloadVideo(link, directory):
+    playlist = Playlist(link)
+    for video in playlist.videos:
+        download_page = downloading(video.title, video.description, video.watch_url, video.thumbnail_url)
+        if "//" in video.title or "/" in video.title or " " in video.title:
+            video.title = video.title.replace(" ", "_").replace("//", "_").replace("/", "_")
+        video.title = video.title.replace(" ", "_").replace("/", "_")
+
+        print(colored('Title : {}\nURL : {}\n\n'.format(video.title, video.watch_url), 'yellow'))
+        video.streams.\
+            filter(type='video', progressive=True, file_extension='mp4').\
+            order_by('resolution').\
+            desc().\
+            first().\
+            download(directory)
+        download_page.close()
+        print(colored("Downloaded : {} with url : {}".format(video.title, video.watch_url), 'green'))
+    sg.Popup("Downloaded all videos in the playlist.")
